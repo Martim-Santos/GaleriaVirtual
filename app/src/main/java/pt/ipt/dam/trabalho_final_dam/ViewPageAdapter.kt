5e52fa69081ba305721a7c2e7d5fc2fee76e425c
DiogoMoinha <aluno23040@ipt.pt>
2024-12-20 17:52:49 +0000

    Recuperação

---
+++ app/src/main/java/pt/ipt/dam/trabalho_final_dam/ViewPageAdapter.kt
@@ -0,0 +1,25 @@
+package pt.ipt.dam.trabalho_final_dam
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import pt.ipt.dam.trabalho_final_dam.fragmentos.fragmento1
+import pt.ipt.dam.trabalho_final_dam.fragmentos.fragmento2
+import pt.ipt.dam.trabalho_final_dam.fragmentos.fragmento3
+
+class ViewPageAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when(position) {
+            0 -> return fragmento1()
+            1 -> return fragmento2()
+            2 -> return fragmento3()
+            else -> return fragmento1()
+        }
+    }
+}
