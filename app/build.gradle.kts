c3dbd6c1f779553ed0a4973bfcd0c215972bd52d
Martim-Santos <128045103+Martim-Santos@users.noreply.github.com>
2025-02-02 16:17:05 +0000

    versão 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "pt.ipt.dam.trabalho_final_dam"
         minSdk = 25
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
